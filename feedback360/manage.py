b1113b088e2b094a7368855c3b3c1efb343e37a4
Minh Duc <81224867+minhhduc99@users.noreply.github.com>
2023-05-30 09:58:31 +0700

    Update manage.py

--- feedback360/manage.py
+++ feedback360/manage.py
@@ -1,22 +1,42 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
-
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feedback360.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
+class ListBatch(views.APIView):
+    pagination_class = BasePagination
+    serializer_class = BatchSerializer
+    @property
+    def paginator(self):
+        if not hasattr(self, '_paginator'):
+            if self.pagination_class is None:
+                self._paginator = None
+            else:
+                self._paginator = self.pagination_class()
+        else:
+            pass
+        return self._paginator
+    def paginate_queryset(self, queryset):
+        
+        if self.paginator is None:
+            return None
+        return self.paginator.paginate_queryset(queryset,
+                   self.request, view=self)
+    def get_paginated_response(self, data):
+        assert self.paginator is not None
+        return self.paginator.get_paginated_response(data)
+    
+    @extend_schema(
+        responses={200: BatchSerializer},
+        description="Description API: List all feedback batches"
+    )
+    def get(self, request, format=None):
+        instance = Batch.objects.all()
+        page = self.paginate_queryset(instance)
+        if page is not None:
+            serializer = self.get_paginated_response(self.serializer_class(page,
+                many=True).data)
+        else:
+            serializer = self.serializer_class(instance, many=True)
+        context = {
+            "message": "Listed Successfully",
+            "status": status.HTTP_200_OK,
+            "data": serializer.data,
+            "errors": None
+        }
+        return Response(context)
